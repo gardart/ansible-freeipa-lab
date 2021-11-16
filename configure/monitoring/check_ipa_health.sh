4af4ae811b6a20b371b27a1383186a9e8cc49bf8
Garðar Þorsteinsson <gardart@gmail.com>
2021-11-16 21:59:31 +0000

    Create check_ipa_health.sh

---
+++ configure/monitoring/check_ipa_health.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+type ipa-healthcheck      >/dev/null 2>&1 || { echo >&2 "CRITICAL: The ipa-healthcheck utility is required for this script to run."; exit 2; }
+
+function usage {
+cat <<EOF
+Usage ./check_ipa_health.sh [-s <Source of checks, e.g. ipahealthcheck.foo.bar>]
+Options:
+  -s <SOURCE>  # Required 
+EOF
+exit 2
+}
+
+while getopts ":s:h" OPTIONS; do
+        case "${OPTIONS}" in
+                s) SOURCE=${OPTARG} ;;
+                h) usage ;;
+                *) usage ;;
+        esac
+done
+
+EXITCODE=0
+
+function returnResult () {
+  CHECKSTATUS="$1"
+  if [[ "$CHECKSTATUS" == "Critical" ]] && [[ $EXITCODE -le 2 ]]; then
+    RESULT=$(echo -e "$CHECKSTATUS: $CHECK\n$RESULT")
+    EXITCODE=2
+  elif [[ "$CHECKSTATUS" == "Warning" ]] && [[ $EXITCODE -ge 2 ]]; then
+    RESULT=$(echo -e "$CHECKSTATUS: $CHECK\n$RESULT")
+    EXITCODE="$EXITCODE"
+  elif [[ "$CHECKSTATUS" == "Warning" ]] && [[ $EXITCODE -le 1 ]]; then
+    RESULT=$(echo -e "$CHECKSTATUS: $CHECK\n$RESULT")
+    EXITCODE=1
+  fi
+}
+
+CHECKS=$(ipa-healthcheck --failures-only --output-type human --source $SOURCE)
+
+IFS=$'\n'
+for CHECK in ${CHECKS}; do
+  case "$CHECK" in
+    CRITICAL:*) returnResult Critical;;
+    ERROR:*) returnResult Critical;;
+    WARNING:*) returnResult Warning;;
+    *) returnResult OK;;
+  esac
+done
+
+case $EXITCODE in
+  0) printf "OK" ;;
+  1) printf "" ;;
+  2) printf "" ;;
+esac
+
+echo "$RESULT"
+exit $EXITCODE
